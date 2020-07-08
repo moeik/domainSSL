2b1e466c5ff2312c8c020537456f613bfd33db6f
moeik <i@aroins.com>
2020-07-08 15:52:59 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# domainSSL
-一键申请域名证书
+# 一键申请Let's Encrypt域名证书
+
+## Only Linux
+
+### 使用:
+
+申请证书前请提前解析域名到服务器
+
+```
+bash <(curl -s -L git.io/dmSSL)
+```
+
+按照提示输入域名即可
---
+++ domainSSL.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+read -p "请输入域名:" domain && cd /tmp
+
+if [ ! -f "lego_v3.8.0_freebsd_amd64.tar.gz" ]; then
+  wget https://cdn.jsdelivr.net/gh/moeik/domainSSL@master/lego/lego_v3.8.0_linux_amd64.tar.gz
+fi
+tar zxvf lego_v3.7.0_linux_amd64.tar.gz
+chmod 755 *
+./lego --email="admin@$domain" --domains="$domain" --http -a run
+
+if ls ./.lego/certificates | grep "$domain"
+    then
+    mkdir /home/ssl/$domain
+    cp ./.lego/certificates/$domain.crt /home/ssl/$domain/$domain.pem
+    cp ./.lego/certificates/$domain.key /home/ssl/$domain/$domain.key
+    path="/home/ssl/$domain/"
+    echo '证书签发成功，证书文件保存在'$path'。'
+else
+    echo '证书签发失败，请检查域名解析或者输入域名是否正确。'
+fi
