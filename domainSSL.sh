578a755f87c7064edbb01bcb8e0d03026378d236
Aroins <i@aroins.com>
2020-09-10 15:09:15 +0800

    Update domainSSL.sh

--- domainSSL.sh
+++ domainSSL.sh
@@ -12,8 +12,8 @@ service nginx start
 if ls ./.lego/certificates | grep "$domain"
     then
     mkdir -p /home/ssl/$domain
-    cp ./.lego/certificates/$domain.crt /home/ssl/$domain/$domain.pem
-    cp ./.lego/certificates/$domain.key /home/ssl/$domain/$domain.key
+    cp ./.lego/certificates/$domain.crt /home/ssl/$domain/1.pem
+    cp ./.lego/certificates/$domain.key /home/ssl/$domain/1.key
     path="/home/ssl/$domain/"
     echo '证书签发成功，证书文件保存在'$path'。'
 else
